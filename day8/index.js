9db9ca19301a23e0e8205c8fd322d206c6f1d215
abhijitmondal121 <mondalavi9836@gmail.com>
2024-02-09 20:51:28 +0530

    9/2/24

---
+++ day8/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+function positiveIntegerHandler(req, res) {
+  const number = req.query.number;
+  if (isNaN(number) || parseInt(number) <= 0) {
+    res.status(400).send('Error: The Number is not a Positive Integer');
+  } else {
+    res.status(200).send(`Varified: The number ${number} is apositive Integer`);
+  }
+}
+
+app.use(positiveIntegerHandler);
+app.get('/positive');
+
+const port = 1800;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
