21e7c0e35894bbfc3ac23a04fd6191983ffb0564
abhijitmondal121 <mondalavi9836@gmail.com>
2024-02-27 22:33:39 +0530

    27/2/24

---
+++ day28/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const WebSocket = require('ws');
+
+function setupWebSocketServer(server) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const wsServer = new WebSocket.Server({ server: httpServer });
+  //const port = 3000;
+
+  const connectedClients = new Set();
+
+  wsServer.on('connection', (socket) => {
+    console.log('New client connected');
+    connectedClients.add(socket);
+
+    socket.on('message', (message) => {
+      console.log('Received message:', message);
+
+      for (const client of connectedClients) {
+        if ((client = socket)) {
+          client.send(message);
+        }
+      }
+    });
+  });
+}
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ day29/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+app.get('/example-route', (req, res, next) => {
+  next(new AppError('Custom error message', 400));
+});
+app.get('/another-route', async (req, res, next) => {
+  try {
+    throw new AppError('Not Found', 404);
+  } catch (error) {
+    next(error);
+  }
+});
+app.get('/yet-another-route', (req, res, next) => {
+  next(new AppError('Internal Server Error', 500));
+});
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  if (err instanceof AppError) {
+    res.status(err.statusCode || 500).json({ error: err.message });
+  } else {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+app.use(errorHandler);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
